59794d2a36dbee279fcc7e957e29ce078a8fa741
Rafael Rabelo <rafael.rabelo@itarget.com.br>
2025-08-04 00:13:15 -0300

    fix: clear old product data and add Cloudinary domain to next.config

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'static.vecteezy.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
   experimental: {
